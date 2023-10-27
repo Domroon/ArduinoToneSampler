a91eaaf3f4132a8b51bfe85da2084d5b8709bde4
Domroon <haeusser-dominik@web.de>
2023-10-27 10:37:38 +0200

    this fixes #1

--- ArduinoToneSampler.ino
+++ ArduinoToneSampler.ino
@@ -1,5 +1,68 @@
-void setup() {
+#define C5 523
+#define Db5 554
+#define D5 587
+#define Eb5 622
+#define E5 659
+#define F5 699
+#define Gb5 740
+#define G5 784
+#define Ab5 831
+#define A5 880
+#define Bb5 932
+#define H5 988
+
+void testToneScale(){
+    tone(8, C5);
+    delay(250);
+    noTone(8); 
     
+    tone(8, Db5); 
+    delay(250); 
+    noTone(8);
+
+    tone(8, D5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, Eb5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, E5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, F5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, Gb5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, G5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, Ab5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, A5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, Bb5);
+    delay(250);
+    noTone(8); 
+
+    tone(8, H5);
+    delay(250);
+    noTone(8); 
+}
+
+void setup() {
+    testToneScale();
 }
 
 void loop() {
